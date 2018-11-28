455ffe6638910d10a5b8dd116d01d35a7c6821e6
Shylock-Hg <tcath2s@gmail.com>
2018-11-28 02:16:41 +0000

    Feature simple guess game program of RUST.

---
+++ guess/src/main.rs
@@ -0,0 +1,44 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+static LOWER : u32 = 0;
+static UPPER : u32 = 99;
+
+fn main() {
+        println!("Hello Guess!");
+
+
+        // generate random target number
+        let target = rand::thread_rng().gen_range(LOWER, UPPER);
+        
+        loop {
+                // read input line
+                println!("Please input your guess:");
+                let mut guess = String::new();
+                io::stdin().read_line(&mut guess)
+                        .expect("Read input failed!");
+                
+                // try to convert input string to number
+                // fail continue
+                let guess: u32 = match guess.trim().parse() {
+                        Ok(num) => num,
+                        Err(_) => continue,
+                };
+                
+                println!("You guess {}!", guess);
+                
+                // compare guess and target
+                //
+                match guess.cmp(&target) {
+                        Ordering::Less => println!("Too less!"),
+                        Ordering::Greater => println!("Too greater!"),
+                        Ordering::Equal => {
+                                println!("Win!");
+                                break;
+                        },
+                };
+        }
+}
