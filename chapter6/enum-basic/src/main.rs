27cd193b107905b03d9c12d3b78b341b9023a3ef
Shylock-Hg <tcath2s@gmail.com>
2018-12-05 09:14:07 +0000

    Feature basic of enum in RUST.

---
+++ chapter6/enum-basic/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+        println!("V4 is {:?}", IpType::v4());
+        let localhost = Ip {kind: IpType::V4, address: String::from("127.0.0.1")};
+        let lo = Ip {kind: IpType::V6, address: String::from("::1")};
+        println!("lo addr is {}", lo.address);
+
+
+        let localhost1 = IpAddr::V4(String::from("127.0.0.1"));
+        let lo1 = IpAddr::V6(String::from("::1"));
+        println!("lo1 is {:?}", lo1);
+
+
+        let msg = Message::Move {x: 2, y: 3};
+        println!("Move msg is {:?}", msg);
+        let msg2 = Message::Quit;
+        println!("Quit msg is {:?}", msg2);
+}
+
+// define enum and using inside struct
+#[derive(Debug)]
+enum IpType{
+        V4,  // unit struct
+        V6,  // unit struct
+}
+
+impl IpType {
+        fn v4() -> IpType {
+                return IpType::V4;  // must specify scope
+        }
+
+        fn v6() -> IpType {
+                return IpType::V6;
+        }
+}
+
+struct Ip {
+        kind: IpType,
+        address: String,
+}
+
+// define enum not in default type
+#[derive(Debug)]
+enum IpAddr {
+        V4(String),  // tuple struct
+        V6(String),  // tuple struct
+}
+
+// define enum in various type
+#[derive(Debug)]
+enum Message {
+    Quit,  // unit struct
+    Move { x: i32, y: i32 },  // struct
+    Write(String),  // tuple struct
+    ChangeColor(i32, i32, i32),  // tuple struct
+}
