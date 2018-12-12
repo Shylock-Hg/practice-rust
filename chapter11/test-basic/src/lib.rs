d3c8fd9f32c7930cb2c277458531060205fd7d6c
Shylock-Hg <tcath2s@gmail.com>
2018-12-12 03:26:27 +0000

    Feature basci test rules in RUST.

---
+++ chapter11/test-basic/src/lib.rs
@@ -0,0 +1,60 @@
+#[cfg(test)]
+mod tests {
+        // panic
+        #[test]
+        fn exploration() {
+                assert_eq!(2 + 2, 4);  // passed
+        }
+
+        #[test]
+        fn ne() {
+                assert_ne!(2 + 3, 4);  // passed
+        }
+
+        #[test]
+        fn another() {
+                assert!(false, "assert!({}) failed!", false);  // failed
+        }
+
+        #[test]
+        fn third() {
+                panic!("panic failed!");  // failed
+        }
+
+        #[test]
+        #[should_panic]
+        fn forth() {  // passed
+                assert!(false);
+        }
+
+        // Result
+        #[test]
+        fn sum() -> Result<(), i32> {  // passed
+                if 2 + 2 == 4 {
+                        Ok(())
+                } else {
+                        Err(-1)
+                }
+        }
+
+        #[test]
+        fn sum2() -> Result<(), i32> {  // failed
+                if 2 + 3 == 4 {
+                        Ok(())
+                } else {
+                        Err(-1)
+                }
+        }
+
+/*
+        #[test]
+        #[should_panic]  // error usage -- only work for panic
+        fn sum3() -> Result<(), i32> {  // error usage
+                if 2 + 3 == 4 {
+                        Ok(())
+                } else {
+                        Err(-1);
+                }
+        }
+*/
+}
