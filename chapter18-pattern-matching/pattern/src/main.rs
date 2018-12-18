accb3e9eb84b0539d91280f040d132e1c4823e08
Shylock-Hg <tcath2s@gmail.com>
2018-12-18 13:20:21 +0000

    Feature all pattern match statement in RUST.

---
+++ chapter18-pattern-matching/pattern/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+        println!("Hello, world!");
+}
+
+// A pattern consists of some combination of the following:
+// Literals
+// Destructured arrays, enums, structs or tuples
+// Variables
+// Wildcards
+// Placeholders
+
+// _ used to ignore pattern
+
+// 1. 
+// match VALUE {
+//      PATTERN => EXPRESSION,
+//      PATTERN => EXPRESSION,
+//      ...
+// }
+
+// 2.
+// if let PATTERN = EXPRESSION
+// if let PATTERN = EXPRESSION else
+// if let PATTERN = EXPRESSION else if PATTERN = EXPRESSION let
+
+// 3.
+// while let PATTERN = EXPRESSION
+
+// 4.
+// for PATTERN in EXPRESSION
+
+// 5.
+// let PATTERN = EXPRESSION;
+
+// 6.
+// function parameters
