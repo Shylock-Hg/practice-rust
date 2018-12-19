b6f1ca0f59cc521d70044dd7ff2bccccfde971d0
Shylock-Hg <tcath2s@gmail.com>
2018-12-19 03:18:25 +0000

    Feature refutability of match pattern in RUST.

---
+++ chapter18-pattern-matching/refutability/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+        // let p = x;  // !! irrefutable
+        // let Some(q) = some_option_value; // !! refutable
+}
+
+// irrefutable -- pattern match any possible cases -- let, for only
+// refutable -- can't match all possible cases -- if let, while let only
